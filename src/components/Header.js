027517baf34b2e06b6b69b1d3d1eed4c0c65ee4b
vnssmorales <vnss.morales@gmail.com>
2019-06-19 09:18:47 -0400

    creando input en header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import logo from '../img/logo.png';
 import '../components/header.css';
 
@@ -10,9 +10,17 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
-            <a href= {imgLogo.baseurl} className= "logo">
-             <img src= {logo} className= "logoImg" />
-            </a>
+                <a href={imgLogo.baseurl} className="logo">
+                    <img src={logo} className="logoImg" />
+                </a>
+
+                <div className="searchBox">
+                <input type="text" value="" placeholder="search" className="searching"></input>
+                <button className="send">Inicio</button>
+ 
+
+                </div>
+
             </div>
         )
     }
