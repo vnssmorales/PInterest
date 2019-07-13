1b26bbfc49beb9e1f862cb363910fac6fff86d71
vnssmorales <vnss.morales@gmail.com>
2019-07-13 19:31:11 -0400

    Updates

---
+++ precache-manifest.a77317df21b8cbf03c5fa57a2b4a4fa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ddbeb6d2d52d9c95901e7ddc2fbe44b",
+    "url": "/Pinterest-Clone/index.html"
+  },
+  {
+    "revision": "3e3a0bbc1e5b66a50b04",
+    "url": "/Pinterest-Clone/static/css/main.2e73459d.chunk.css"
+  },
+  {
+    "revision": "69e05fd7cf98a5f917f7",
+    "url": "/Pinterest-Clone/static/js/2.d5c7d8b4.chunk.js"
+  },
+  {
+    "revision": "3e3a0bbc1e5b66a50b04",
+    "url": "/Pinterest-Clone/static/js/main.fa29f989.chunk.js"
+  },
+  {
+    "revision": "8079d92e218ba437ef96",
+    "url": "/Pinterest-Clone/static/js/runtime~main.4f0434c0.js"
+  },
+  {
+    "revision": "899cd5e1bc97ea4c94c0b088b0fa23d8",
+    "url": "/Pinterest-Clone/static/media/logo.899cd5e1.png"
+  }
+]);
